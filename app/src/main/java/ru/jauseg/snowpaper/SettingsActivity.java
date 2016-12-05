21bfd77568b0c86514623af13486ef9f5a1d99ea
jauseg <jauseg@gmail.com>
2016-12-05 21:09:24 +0300

    added settings layout

---
+++ app/src/main/java/ru/jauseg/snowpaper/SettingsActivity.java
@@ -0,0 +1,14 @@
+package ru.jauseg.snowpaper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_settings);
+	}
+}
